57979bb9862544c553955d864a13cfb9219ca514
Jiri Vinduska <vinduskajiri@gmail.com>
2022-02-24 08:28:01 +0100

    add status api

---
+++ pages/api/status.ts
@@ -0,0 +1,26 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { Weather } from '../../src/entity/weather.model';
+import { findLast } from '../../src/repository/weather.repository';
+import os from 'os';
+
+
+interface StatusResponse {
+    status: 'ALIVE'
+    uptime: number;
+    uptimeHours: number;
+    date: Date;
+}
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<StatusResponse>
+) {
+    const osUptime = os.uptime()
+    res.status(200).json({
+        status: 'ALIVE',
+        uptime: osUptime,
+        uptimeHours: osUptime / 60 / 60,
+        date: new Date()
+    });
+
+}
