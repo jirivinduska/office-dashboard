599e2621abe321984c0a2fb758283a0419baac16
Jiri Vinduska <vinduskajiri@gmail.com>
2022-03-09 09:02:42 +0100

    not null

--- prisma/migrations/20220309070136_init/migration.sql
+++ prisma/migrations/20220309070136_init/migration.sql
@@ -16,7 +16,7 @@ CREATE TABLE `Weather` (
     `cpuTemp` DECIMAL(6, 2) NOT NULL,
     `pressure` DECIMAL(6, 2) NOT NULL,
     `humidity` DECIMAL(6, 2) NOT NULL,
-    `created` DATETIME(3) NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `created` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
