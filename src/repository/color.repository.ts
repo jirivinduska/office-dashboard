180bb94e9404f5b16808acc99b69d00ef6ba696a
Jiri Vinduska <vinduskajiri@gmail.com>
2022-03-09 09:16:58 +0100

    update color

--- src/repository/color.repository.ts
+++ src/repository/color.repository.ts
@@ -1,10 +1,14 @@
-import { Color } from '@prisma/client';
-import { prisma } from '../database/db'
+import { Color } from "@prisma/client";
+import { prisma } from "../database/db";
 
-export const findLast = async (): Promise<Color| null> => {
+export const findLast = async (): Promise<Color | null> => {
   return prisma.color.findFirst({ orderBy: { created: "desc" } });
 };
 
 export const addColor = async (colorHex: string): Promise<Color> => {
-  return prisma.color.create({ data: { colorHex: colorHex } });
+  return prisma.color.upsert({
+    where: { id: 1 },
+    update: { colorHex: colorHex },
+    create: { colorHex: colorHex },
+  });
 };
